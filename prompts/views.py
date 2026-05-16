db0e63f96fc52e50e6a5f6240e69bd7c8de195d7
Anton Tsikhanovich <alluspeh@gmail.com>
2026-05-16 19:37:17 +0200

    search added to PromptListView

--- prompts/views.py
+++ prompts/views.py
@@ -5,6 +5,7 @@ from django.db.models import Avg
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from prompts.forms import SearchForm
 from prompts.models import Prompt, Author, Category
 from django.views import generic
 from django.db.models.functions import Round
@@ -23,13 +24,21 @@ def index(request: HttpRequest) -> HttpResponse:
 class PromptListView(generic.ListView):
     model = Prompt
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PromptListView, self).get_context_data(**kwargs)
+        title = self.request.GET.get("title", "")
+        context["search_form"] = SearchForm(initial={"title": title})
+        return context
+
     def get_queryset(self):
-        return (
-            Prompt.objects
-            .select_related("author")
-            .prefetch_related("categories")
-            .annotate(avg_rating=Round(Avg("ratings__value"), 2))
-        )
+        queryset = (Prompt.objects.select_related("author")
+                    .prefetch_related("categories")
+                    .annotate(avg_rating=Round(Avg("ratings__value"),
+                                               2)))
+        form = SearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(title__icontains=form.cleaned_data["title"])
+        return queryset
 
 
 class AuthorListView(generic.ListView):
