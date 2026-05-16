896d22324a0bb7f35800e5d136d7915f960c3086
Anton Tsikhanovich <alluspeh@gmail.com>
2026-05-16 17:22:05 +0200

    listView for Prompt model added

--- prompts/urls.py
+++ prompts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from prompts.views import index
+from prompts.views import index, PromptListView
+
 
 urlpatterns = [
     path("", index, name="index"),
+    path("prompts/", PromptListView.as_view(), name="prompt-list"),
 ]
 
 app_name = "prompts"
--- prompts/views.py
+++ prompts/views.py
@@ -1,7 +1,33 @@
+from gc import get_objects
+from multiprocessing import context
+
+from django.db.models import Avg
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from prompts.models import Prompt, Author, Category
+from django.views import generic
+from django.db.models.functions import Round
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "prompts/index.html")
+    num_prompts = Prompt.objects.all().count()
+    num_authors = Author.objects.all().count()
+    num_categories = Category.objects.all().count()
+    context = {
+        "num_prompts": num_prompts,
+        "num_authors": num_authors,
+        "num_categories": num_categories,
+    }
+    return render(request, "prompts/index.html", context=context)
+
+class PromptListView(generic.ListView):
+    model = Prompt
+
+    def get_queryset(self):
+        return (
+            Prompt.objects
+            .select_related("author")
+            .prefetch_related("categories")
+            .annotate(avg_rating=Round(Avg("ratings__value"), 2))
+        )
 
